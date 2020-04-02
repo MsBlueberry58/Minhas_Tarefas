115afdb93d420187057b7d8769248079e868ecf6
Bruna Guedes <57498093+MsBlueberry58@users.noreply.github.com>
2020-04-01 21:01:20 -0300

    top top

--- Compiladores/Prolog/familia.pl
+++ Compiladores/Prolog/familia.pl
@@ -19,27 +19,22 @@ mulher(maria) .
 % REGRAS
 % Regra: filho(X)
 % Significado: X é filho de Y se Y é pai de X e X é homem ou se Y é mãe de X e X é homem
-filho(X) :- pai(Y, X), homem(X) .
-filho(X) :- mae(Y, X), homem(X) .
+filho(X,Y):-pai(Y, X); mae(Y, X), homem(X) .
 
 % Regra: filha(X)
 % Significado: X é filha de Y se Y é pai de X e X é mulher ou X é filha de Y se Y é mãe de X e X é mulher
-filha(X):  -pai(Y, X), mulher(X) ;
-filha(X):  -mae(Y, X), mulher(X) .
+filha(X, Y):-pai(Y, X); mae(Y, X) mulher(X) .
 
 % Regra: umpai(Y)
 % Significado: Y é um dos pais de X se Y é pai de X ou se Y é mãe de X
-umpai(Y): -pai(Y, X) ;
-umpai(Y): -mae(Y, X) . 
+umpai(Y, X):-pai(Y, X) ; mae(Y, X) . 
 
 % Regra: umavo(X)
 % Significado: X é um avô ou avó se X for um dos pais de alguém que é um dos pais de alguma outra
-umavo(X): -umpai(umpai(_)), mulher(X) ;
-umavo(X): -umpai(umpai(_)), homem(X) .
+umavo(X):-umpai(X, Y), umpai(Y, Z) .
 
 % Regra: ancestral(X)
 % Significado: X é ancestral de Y se X for um dos pais de Y ou se X for um dos pais de alguém que seja ancestral de Y
-ancestral(X): -umpai(Y) ;
-ancestral(X): -umpai(umpai(_)) .
+ancestral(X, Y):-umpai(Y, X), umpai(Y, Z) .
 
  
